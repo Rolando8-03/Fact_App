a2c7d8e80217c830ac18d5d223c1ca0d788814f4
Rolando8 <remena@uamv.edu.ni>
2026-09-08 16:23:12 -0600

    update Cargo and Empleado

--- src/main/java/ni/edu/uam/fact_app/model/Cargo.java
+++ src/main/java/ni/edu/uam/fact_app/model/Cargo.java
@@ -1,4 +1,18 @@
 package ni.edu.uam.fact_app.model;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor
+
 public class Cargo {
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaContratacion;
+    private boolean activo;
+
 }
--- src/main/java/ni/edu/uam/fact_app/model/Empleado.java
+++ src/main/java/ni/edu/uam/fact_app/model/Empleado.java
@@ -1,4 +1,11 @@
 package ni.edu.uam.fact_app.model;
 
+import lombok.*;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor
 public class Empleado {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
 }
