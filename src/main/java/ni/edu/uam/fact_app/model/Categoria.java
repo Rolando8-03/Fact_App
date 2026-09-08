ab5ee014e9608b6a929d6ad1cc12d203e1f86e25
Rolando8 <remena@uamv.edu.ni>
2026-09-08 16:01:51 -0600

    feat: se agrega class Producto.java y Categoria.java

--- src/main/java/ni/edu/uam/fact_app/model/Categoria.java
+++ src/main/java/ni/edu/uam/fact_app/model/Categoria.java
@@ -1,4 +1,17 @@
 package ni.edu.uam.fact_app.model;
 
+import lombok.*;
+
+@Data @NoArgsConstructor
+@AllArgsConstructor
+
 public class Categoria {
+    private Integer id;
+    private String nombre;
+    private boolean activa;
+
+    @Override
+    public String toString(){
+        return nombre;
+    }
 }
